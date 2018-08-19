d0c88e4f96fba81826f938b8cbbce4de2636cbe1
gregorylaksono <greg.laksono@gmail.com>
2018-08-19 17:16:22 +0700

    add glify diagram

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { UtilService } from './services/util.service';
+import { CompanyInfo } from './model/companyinfo';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+  companyInfo:CompanyInfo;
+  constructor(private utilService:UtilService){
+    this.companyInfo = this.utilService.getCompanyInfo();
+  }
+
 }
